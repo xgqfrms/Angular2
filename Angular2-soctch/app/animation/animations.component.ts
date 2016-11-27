238a2c1fd5e0cd269b6266e82e8192b892bbfd6b
xgqfrms <xgqfrms@outlook.com>
2016-11-28 04:55:31 +0800

    OK

--- Angular2-soctch/app/animation/animations.component.ts
+++ Angular2-soctch/app/animation/animations.component.ts
@@ -16,18 +16,35 @@ import {
     animations:[
         trigger('focusPanel', [
             state('inactive', style({
-                transform: "scale(2)"
+                transform: "scale(1)",
+                background: "rgba(0,255,0,0.7)"
             })),
             state('active', style({
-                 transform: "scale(1)"
+                transform: "scale(1.5)",
+                top: "100px",
+                background: "rgba(255,0,255,0.7)"
             })),
-            transition('inactive => active', animate('all 100')),
-            transition('inactive => active', animate('all 100'))
+            transition('inactive => active', animate('100ms ease-in')),
+            transition('active => inactive', animate('100ms ease-out'))
+        ]),
+        trigger('movePanel', [
+            transition('void => *', [
+                animate(600, keyframes([
+                    style({opacity: 0, transform: 'translateY(-200px)', offset: 0}),
+                    style({opacity: 1, transform: 'translateY(25px)', offset: .75}),
+                    style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+                ]))
+            ])
         ])
     ]
 })
 
-export class AnimationsComponent {}
+export class AnimationsComponent {
+    state: string = "inactive";
+    toggleMove = () => {
+        this.state = (this.state === "inactive" ? "active" : "inactive");
+    }
+}
 
 /**
  * https://www.youtube.com/watch?v=-pFEzm_Jn-o
@@ -57,6 +74,13 @@ export class AnimationsComponent {}
  *
  * REST-API 
  * http://reqres.in
+ *
+ * https://coursetro.com/posts/code/18/Angular-2-Templates-&-Integrating-Foundation-&-Bootstrap  
+ *
+ * http://www.w3schools.com/css/css3_animations.asp
+ * http://www.w3schools.com/cssref/css3_pr_animation.asp
+ * http://www.w3schools.com/cssref/css3_pr_transition.asp
+ * http://www.w3schools.com/cssref/css3_pr_transition-property.asp
  */
 
 
--- Angular2-soctch/dist/animation/animations.component.js
+++ Angular2-soctch/dist/animation/animations.component.js
@@ -11,6 +11,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AnimationsComponent = (function () {
     function AnimationsComponent() {
+        var _this = this;
+        this.state = "inactive";
+        this.toggleMove = function () {
+            _this.state = (_this.state === "inactive" ? "active" : "inactive");
+        };
     }
     AnimationsComponent = __decorate([
         core_1.Component({
@@ -19,14 +24,26 @@ var AnimationsComponent = (function () {
             styleUrls: ['../../public/css/animation.css'],
             animations: [
                 core_1.trigger('focusPanel', [
-                    core_1.state('inactive', style({
-                        transform: "scale(2)"
+                    core_1.state('inactive', core_1.style({
+                        transform: "scale(1)",
+                        background: "rgba(0,255,0,0.7)"
                     })),
-                    core_1.state('active', style({
-                        transform: "scale(1)"
+                    core_1.state('active', core_1.style({
+                        transform: "scale(1.5)",
+                        top: "100px",
+                        background: "rgba(255,0,255,0.7)"
                     })),
-                    core_1.transition('inactive => active', core_1.animate('all 100')),
-                    core_1.transition('inactive => active', core_1.animate('all 100'))
+                    core_1.transition('inactive => active', core_1.animate('100ms ease-in')),
+                    core_1.transition('active => inactive', core_1.animate('100ms ease-out'))
+                ]),
+                core_1.trigger('movePanel', [
+                    core_1.transition('void => *', [
+                        core_1.animate(600, core_1.keyframes([
+                            core_1.style({ opacity: 0, transform: 'translateY(-200px)', offset: 0 }),
+                            core_1.style({ opacity: 1, transform: 'translateY(25px)', offset: .75 }),
+                            core_1.style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+                        ]))
+                    ])
                 ])
             ]
         }), 
@@ -37,6 +54,22 @@ var AnimationsComponent = (function () {
 exports.AnimationsComponent = AnimationsComponent;
 /**
  * https://www.youtube.com/watch?v=-pFEzm_Jn-o
+ * https://coursetro.com/posts/code/25/Understanding-Angular-2-Animations-Tutorial
+ *
+ * EXPERIMENTAL (试验性)
+ * https://angular.io/docs/ts/latest/api/core/index/state-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/style-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/trigger-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/transition-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/keyframes-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/animate-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/sequence-function.html
+ * https://angular.io/docs/ts/latest/api/core/index/group-function.html
+ *
+ * Metadata Properties:
+ * styleUrls, templateUrl, animations, providers, selector ...
+ *
+ * https://angular.io/docs/ts/latest/api/core/index/Component-decorator.html
  *
  * https://github.com/angular/angular/blob/2.2.2/modules/%40angular/core/src/metadata/directives.ts#L694-L715
  * template & templateUrl
@@ -47,5 +80,12 @@ exports.AnimationsComponent = AnimationsComponent;
  *
  * REST-API
  * http://reqres.in
+ *
+ * https://coursetro.com/posts/code/18/Angular-2-Templates-&-Integrating-Foundation-&-Bootstrap
+ *
+ * http://www.w3schools.com/css/css3_animations.asp
+ * http://www.w3schools.com/cssref/css3_pr_animation.asp
+ * http://www.w3schools.com/cssref/css3_pr_transition.asp
+ * http://www.w3schools.com/cssref/css3_pr_transition-property.asp
  */
 //# sourceMappingURL=animations.component.js.map
