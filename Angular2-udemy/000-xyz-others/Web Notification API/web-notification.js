aeec7092ba59a22fa8b660d6af4811e7598ea092
xgqfrms <xgqfrms@outlook.com>
2016-12-09 08:21:46 +0800

    againts regular webfonts
    againts regular webfonts
    https://javascript30.com/

--- Angular2-udemy/000-xyz-others/Web Notification API/web-notification.js
+++ Angular2-udemy/000-xyz-others/Web Notification API/web-notification.js
@@ -11,14 +11,26 @@
         if (permissionx === "denied" || permissionx === "granted") {
             return ;
         }
+        let latestVersion = document.querySelector(".js-currentVersion").textContent;
         // Notification.requestPermission();
         // Promise {[[PromiseStatus]]: "pending", [[PromiseValue]]: undefined}
         Notification
             .requestPermission()
             .then(() => {
-                let notification = new Notification("Hello, world!");
-                alert(notification.title);
-                console.log(notification);
+                let title = `Click to see what's new in v${latestVersion}`;
+                let options = {
+                    body: `https://github.com/xgqfrms/xgqfrms/releases/v${latestVersion}`,
+                    icon: "http://www.xgqfrms.xyz/images/icon.png",
+                    link: `https://github.com/xgqfrms/xgqfrms/releases/v${latestVersion}`
+                };
+                var notification = new Notification(title, options);
+                if (ture) {
+                    notification.onclick = () => {
+                        window.open(`https://github.com/xgqfrms/xgqfrms/releases/`);
+                    };
+                }
+                setTimeout(notification.close.bind(notification), 5000);
+                //alert(`title = ${notification.title}\n notification.options = ${notification.options}`);
                 console.log(notification.title);
             });
     }else{
@@ -37,16 +49,31 @@ var n = new Notification(theTitle,options);
 */
 
 
-let checkVersion = () => {
-    let latestVersion = document.querySelector(".js-currentVersion").textContent;
-    let currentVersion = localStorage.getItem("conciseVersion");
-    if (currentVersion === null || currentVersion < latestVersion ) {
-        let notification = new Notification("Hello, world!");
-        localStorage.setItem("conciseVersion", latestVersion);
+ 
+
+(() => {
+    // let is only available in {}
+    if ("Notification" in window) {
+        console.log("Web Notification API is supported by this browser!");
+        let permissionx = Notification.permission;
+        console.log(`permissionx = ${permissionx}`);
+        if (permissionx === "denied" || permissionx === "granted") {
+            return ;
+        }
+        // Notification.requestPermission();
+        // Promise {[[PromiseStatus]]: "pending", [[PromiseValue]]: undefined}
+        Notification
+            .requestPermission()
+            .then(() => {
+                let notification = new Notification("Hello, world!");
+                alert(notification);
+            });
+    }else{
+        console.log("Your old browser doesn't support Web Notification API!");
     }
-    return latestVersion;
-}
-checkVersion();
+})();
+
+
 
 let displayNotification = (body, icon, title="Web Notification API", link="www.xgqfrms.xyz", duration=5000) => {
     link = link;
@@ -64,13 +91,11 @@ let displayNotification = (body, icon, title="Web Notification API", link="www.x
     setTimeout(n.close.bind(n), duration);
 }
 
-http://www.xgqfrms.xyz/images/icon.png
 
 
-function checkVersion() {
+let checkVersion = () => {
     let latestVersion = document.querySelector(".js-currentVersion").textContent;
     let currentVersion = localStorage.getItem("conciseVersion");
-
     if (currentVersion === null || currentVersion < latestVersion ) {
         displayNotification(
             `Click to see what's new in v${latestVersion}`,
