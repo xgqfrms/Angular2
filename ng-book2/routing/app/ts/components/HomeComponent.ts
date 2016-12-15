638fa5283c00f00fc984b13314c39ca8e2e40656
xgqfrms <xgqfrms@outlook.com>
2016-12-15 09:57:34 +0800

    routing-app

---
+++ ng-book2/routing/app/ts/components/HomeComponent.ts
@@ -0,0 +1,11 @@
+/*
+ * Angular
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    template: `<h1>Welcome to Home!</h1>`
+})
+
+export class HomeComponent { }
